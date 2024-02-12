0728c9e44250a1709c6b75542bf4c75341a45821
AndresMarolt <andresmmarolt@gmail.com>
2024-02-12 19:03:37 +0100

    Add superhero interface and add httpClient provider in app.config.ts

---
+++ src/app/models/superhero.interface.ts
@@ -0,0 +1,10 @@
+export interface Superhero {
+  id?: number;
+  name: string;
+  alias: string;
+  age: number;
+  height: number;
+  weight: number;
+  superpowers: string[];
+  image?: string[];
+}
